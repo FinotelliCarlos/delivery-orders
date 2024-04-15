5a84866dfd95f7a86b15a6f55f540771f514fc16
finotellicarlos <carlos.finotelli@outlook.com>
2024-04-15 14:23:13 -0300

    feat: implements async storage with zustend middleware

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export interface ProductCartProps extends ProductProps {
@@ -13,15 +15,23 @@ interface StateProps {
   remove: (productId: string) => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
 
-  remove: (productId: string) =>
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    })),
-}));
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "delivery-orders:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
